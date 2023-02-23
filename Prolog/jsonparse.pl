f43265f109845fd88942d35aea3a80ca7572d54f
Luca Brini <luca.brini002@gmail.com>
2023-02-23 19:07:35 +0100

    implemented jsondump predicate

--- Prolog/jsonparse.pl
+++ Prolog/jsonparse.pl
@@ -64,8 +64,8 @@ format_string(String) -->
     chars(Chars),
     {Chars \= []},                          % checking if string is not empty
     {
-        atom_chars(RawAtomString, Chars),       % converting chars list to atom 
-        string_to_atom(String, RawAtomString)   % string and then to Prolog string
+        atom_chars(RawAtom, Chars),       % converting chars list to atom 
+        string_to_atom(String, RawAtom)   % and then to Prolog string
     }.  
 
 % stacking up chars of a string recursively using DGCs.
@@ -163,47 +163,64 @@ jsonread(FileName, JSON) :-
 jsondump(JSON, FileName) :-
     jsonreverse(JSON, JSONAtom),
     string_to_atom(JSONString, JSONAtom),
-    print(JSONString),
     open(FileName, write, Stream),
     write(Stream, JSONString),
     close(Stream).
     
 % JSONReverse
+% Handling case where object is a jsonobj
 jsonreverse(jsonobj(Members), AtomString) :-
-    print("jsonobject"),
     jsonreverse_members(Members, IntermediateString),
-    concat_atom(['{', IntermediateString, '}'], AtomString).
-
+    concat_atom(['{', IntermediateString, '}'], AtomString),
+    !.
+% Handling case where object is a jsonarray
 jsonreverse(jsonarray(Elements), AtomString) :-
-    print("jsonarray"),
     jsonreverse_elements(Elements, IntermediateString),
-    concat_atom(['[', IntermediateString, ']'], AtomString).
+    concat_atom(['[', IntermediateString, ']'], AtomString),
+    !.
 
+% Members obj --> Member string : Base case
 jsonreverse_members([], '').
-jsonreverse_members([(RawKey, Value) | []], AtomString) :-
-    jsonreverse(RawKey, Key),
-    jsonreverse(Value, IntermediateString),
-    concat_atom([Key, ':', IntermediateString], AtomString),
-    print(StringAtom).
-
+% Members obj --> Member string : only one pair
+jsonreverse_members([Pair | []], AtomString) :-
+    jsonreverse_pair(Pair, AtomString).
+% Members obj --> Member string : More than one Pair
 jsonreverse_members([Pair | MoreMembers], AtomString) :-
-    jsonreverse_members(Pair, PairString),
+    jsonreverse_pair(Pair, PairString),
     jsonreverse_members(MoreMembers, MoreMembersString),
-    print(MoreMembersString),
-    concat_atom([PairString, ',', MoreMembersString], AtomString),
-    print(AtomString).
+    concat_atom([PairString, ',', MoreMembersString], AtomString).
 
+% Pair obj --> Pair string : Only one Pair
+jsonreverse_pair((RawKey, Value), AtomString) :-
+    jsonreverse_value(RawKey, Key),
+    jsonreverse_value(Value, IntermediateString),
+    concat_atom([Key, ':', IntermediateString], AtomString).
+
+% Reversing elements: from list of values to string 
 jsonreverse_elements([], '').
-jsonreverse_elements([Element | []], String) :-
-    jsonreverse(Element, IntermediateString).
-jsonreverse_elements([Element, MoreElements], String) :-
-    jsonreverse(Element, IntermediateString1),
-    jsonreverse_elements(MoreElements, IntermediateString2).
+jsonreverse_elements([Element | []], AtomString) :-
+    jsonreverse_value(Element, AtomString).
+jsonreverse_elements([Value | MoreElements], AtomString) :-
+    jsonreverse_value(Value, ValueString),
+    jsonreverse_elements(MoreElements, MoreElementsString),
+    concat_atom([ValueString, ',', MoreElementsString], AtomString).
 
-jsonreverse(RawString, String) :-
-    string(RawString),
-    concat_atom(['\"', RawString, '\"'], String).
+jsonreverse_value(false, false).
+jsonreverse_value(true, true).
+jsonreverse_value(null, null).
+% JSONObject value is a string.
+jsonreverse_value(JSONString, String) :-
+    string(JSONString),
+    concat_atom(['\"', JSONString, '\"'], String),
+    !.
+% Handling the case where JSONObject value is a number.
+jsonreverse_value(JSONNumber, String) :-
+    number(JSONNumber),
+    concat_atom([JSONNumber], String),
+    !.
+% Handling the case where JSONObject value is a composite object
+jsonreverse_value(JSONObj, String) :- 
+    jsonreverse(JSONObj, String),
+    !.
 
 reload :- [jsonparse], tty_clear.
-
-
